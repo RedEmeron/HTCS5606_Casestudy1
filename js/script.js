4bbcabedf61f5f0040c7c3bf6b1437c2b34c150d
David Kadlcik <david.kadlcik@gmail.com>
2020-05-15 23:32:11 +1200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function purchaseItem(e) {
+
+    cart = document.getElementById("totalPrice").textContent;
+    display = document.getElementById("totalPrice");
+    price =
+    display.innerText = cart + price;
+    switch (e.id) {
+        case "dogFoodA":
+            quantity = document.getElementById("dogFoodQuantity")
+
+
+    }
+
+}
+
+// function logoAnimation() {
+//     logo = document.getElementById("logo");
+//     x = 0;
+//     speed = 1;
+//     setInterval(function () {
+//         x=x+speed;
+//         console.log("x=", x);
+//         logo.style.left = x+"px";
+//     },15)
+// }
